dcdca4622739e71cd952ffc1ed0ab5d6a76f6ed2
Pavel Serikov <pavel.p.serikov@gmail.com>
2022-12-27 23:29:17 +0300

    Added encode audio

---
+++ encode_audio.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+raw_voice_dir=audio_src
+mono_voice_dir=audio_1ch
+
+mkdir -p $mono_voice_dir
+
+for entry in "$raw_voice_dir"/*
+do
+  nvbase="$(basename $entry)"
+  ffmpeg -i "$entry" -ac 1 "$mono_voice_dir/$nvbase"
+done
